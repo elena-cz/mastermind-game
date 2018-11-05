71889411bff5bd814bf1c9b2e481627c5ed778f2
Elena Czubiak <elenaczubiak@gmail.com>
2018-11-05 14:05:50 -0800

    Update color picker to use path based on adjustable variables

--- src/components/ColorPicker/ColorPicker.js
+++ src/components/ColorPicker/ColorPicker.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Container, CurvedContainer, CurvedOffset, CircleSVG, CircleBackground, Peg} from './styled';
+import { Container, ViewBox, CurvePath, ColorDot, Peg } from './styled';
 
 function ColorPicker(props) {
   return (
     <Container>
-      <CircleSVG>
-        <CircleBackground />
-      </CircleSVG>
+      <ViewBox>
+        <CurvePath />
+      </ViewBox>
+      <ColorDot distance={0} />
+      <ColorDot distance={20} />
+      <ColorDot distance={40} />
+      <ColorDot distance={60} />
+      <ColorDot distance={80} />
+      <ColorDot distance={100} />
       <Peg color="purple" />
     </Container>
   );
@@ -16,14 +22,3 @@ function ColorPicker(props) {
 ColorPicker.propTypes = {};
 
 export default ColorPicker;
-
-// <CurvedContainer viewBox="0 0 128 64">
-//         <path
-//           d="M27.8,41.1a41,41,0,0,1,6.5-8.4A41.9,41.9,0,0,1,64,20.4m36.2,20.7a41,41,0,0,0-6.5-8.4A41.9,41.9,0,0,0,64,20.4"
-//           className="path"
-//           fill="none"
-//           strokeWidth="32px"
-//         />
-//       </CurvedContainer>
-
-//       <CurvedOffset />
