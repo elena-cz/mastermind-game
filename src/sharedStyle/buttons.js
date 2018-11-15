5c0e6e181f13d6e0b165e7681f7860c94a4f44f6
Elena Czubiak <elenaczubiak@gmail.com>
2018-11-15 14:28:11 -0800

    Add primary and round buttons to shared styles

--- src/sharedStyle/buttons.js
+++ src/sharedStyle/buttons.js
@@ -1,14 +1,41 @@
 import styled from 'styled-components/macro';
 import media from 'sharedStyle/utils/media';
 
+const PrimaryButton = styled.button`
+  padding: 0.5em 1.5em;
+  background-color: ${props => props.theme.colors.black};
+  border: 1px solid ${props => props.theme.colors.black};
+  border-radius: 1em;
+  font-size: 1.5em;
+  text-transform: uppercase;
+  color: white;
+
+  &:hover {
+
+  }
+  &:focus {
+  }
+
+  ${media.tablet`
+    font-size: 1.2em;
+    padding: 0.5em 1.25em;
+  `}
+  ${media.phone`
+    font-size: 1em;
+    padding: 0.4em 1em;
+  `}
+`;
+
+
 const OutlineButton = styled.button`
   padding: 0.25em 1em;
   background-color: white;
-  border: 1px solid ${props => props.theme.colors.primary};
+  box-sizing: border-box;
+  border: 1px solid ${props => props.theme.colors.black};
   border-radius: 1em;
   font-size: 1.2em;
   text-transform: uppercase;
-  color: ${props => props.theme.colors.primary};
+  color: ${props => props.theme.colors.black};
 
   &:hover {
     background-color: ${props => props.theme.colors.lightPrimary};
@@ -27,4 +54,14 @@ const OutlineButton = styled.button`
   `}
 `;
 
-export { OutlineButton };
+const RoundButton = styled.button`
+  padding: 0.5em .5em;
+  background-color: ${props => props.theme.colors.white};
+  border: 1px solid ${props => props.theme.colors.black};
+  border-radius: 50%;
+  font-size: 1.5em;
+  text-transform: uppercase;
+  color: ${props => props.theme.colors.black};
+`;
+
+export { PrimaryButton, OutlineButton };
