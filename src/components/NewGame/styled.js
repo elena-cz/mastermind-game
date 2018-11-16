83b85e1d7b70bd4167146ad779ab705f042368de
Elena Czubiak <elenaczubiak@gmail.com>
2018-11-16 15:56:10 -0800

    Fix button hover colors

--- src/components/NewGame/styled.js
+++ src/components/NewGame/styled.js
@@ -17,6 +17,11 @@ const NumPegButton = styled(RoundButton)`
   margin-right: ${props => props.theme.spacers.sm};
   background-color: ${props => props.isCurrentNumPegs ? props.theme.colors.black : props.theme.colors.white};
   color: ${props => props.isCurrentNumPegs ? props.theme.colors.white : props.theme.colors.black};
+
+  &:hover, &:focus {
+    background-color: ${props => props.isCurrentNumPegs ? props.theme.colors.black : props.theme.colors.lightPrimary};
+    color: ${props => props.isCurrentNumPegs ? props.theme.colors.lightPrimary : props.theme.colors.black};
+  }
 `;
 
 const Description = styled.p`
