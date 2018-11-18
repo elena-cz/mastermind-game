8b5425fd72d1a22bea0886c9e9abb6c5c07a3d3d
Elena Czubiak <elenaczubiak@gmail.com>
2018-11-17 22:00:26 -0800

    Updates

---
+++ precache-manifest.7de4ba01402d2e1fb26668a1a8bee150.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "61689cf2e9b098aa3ea00eeb3336e213",
+    "url": "/mastermind-game/static/media/visibility-icon.61689cf2.svg"
+  },
+  {
+    "revision": "b7400197b452aba45e20c06d1cc14961",
+    "url": "/mastermind-game/static/media/expand-less-icon.b7400197.svg"
+  },
+  {
+    "revision": "2e773b1da7991b769fbd",
+    "url": "/mastermind-game/static/js/runtime~main.2e773b1d.js"
+  },
+  {
+    "revision": "3398a0978495d2ca3425",
+    "url": "/mastermind-game/static/js/main.3398a097.chunk.js"
+  },
+  {
+    "revision": "a0adf1bcaf05bbf184a8",
+    "url": "/mastermind-game/static/js/1.a0adf1bc.chunk.js"
+  },
+  {
+    "revision": "ed85e840ac4db081e52e4a85e6279ba2",
+    "url": "/mastermind-game/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/mastermind-game/precache-manifest.7de4ba01402d2e1fb26668a1a8bee150.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/mastermind-game/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
